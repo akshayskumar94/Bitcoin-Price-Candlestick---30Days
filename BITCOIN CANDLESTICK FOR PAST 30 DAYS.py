174b2cfb4d20e311f71af75f2aa9318c44fd0523
akshayskumar94 <79533562+akshayskumar94@users.noreply.github.com>
2021-03-10 23:19:55 +0530

    Update BITCOIN CANDLESTICK FOR PAST 30 DAYS.py
    Modified formatting

--- BITCOIN CANDLESTICK FOR PAST 30 DAYS.py
+++ BITCOIN CANDLESTICK FOR PAST 30 DAYS.py
@@ -1,98 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[1]:CODE TO OBTAIN CANDLE STICK PLOT TO GET PRICE FLUCTUATION ON BITCOIN FOR PAST 30 DAYS
 
 
-get_ipython().system('pip install pycoingecko')
-
-
-# In[2]:
-
-
-from pycoingecko import CoinGeckoAPI
-
-
-# In[3]:
-
+get_ipython().system('pip install pycoingecko') #INSTALL pycoingecko
+from pycoingecko import CoinGeckoAPI #Import CoinGecko API
 
 cg=CoinGeckoAPI()
 
-
-# In[4]:
-
-
 bitcoin_data=cg.get_coin_market_chart_by_id(id='bitcoin',vs_currency='usd',days=30)
-
-
-# In[24]:
-
 
 bitcoin_price_data=bitcoin_data['prices']
 
-
-# In[6]:
-
-
 import pandas as pd
-
-
-# In[25]:
-
 
 data=pd.DataFrame(bitcoin_price_data, columns=['TimeStamp','Price'])
 
-
-# In[26]:
-
-
-data
-
-
-# In[27]:
-
+print(data)
 
 data['Date'] = pd.to_datetime(data['TimeStamp'],unit='ms')
 
-
-# In[28]:
-
-
-data
-
-
-# In[29]:
-
+print(data0
 
 candlestick_data = data.groupby(data.Date.dt.date).agg({'Price':['min','max','first','last']})
 
-
-# In[30]:
-
-
 candlestick_data
-
-
-# In[35]:
-
 
 get_ipython().system('pip install plotly')
 
-
-# In[50]:
-
-
 import plotly.graph_objects as go
-
-
-# In[48]:
-
-
-dir(matplotlib.pyplot)
-
-
-# In[61]:
-
 
 fig=go.Figure(data=[go.Candlestick(x= candlestick_data.index, 
                                    open=candlestick_data['Price']['first'], 
@@ -101,22 +38,11 @@ fig=go.Figure(data=[go.Candlestick(x= candlestick_data.index,
                                    close=candlestick_data['Price']['last'])])
 
 
-# In[62]:
-
-
-fig
-
-
-# In[60]:
-
+print(fig)
 
 fig.update_layout(xaxis_rangeslider_visible=False, xaxis_title='Date',
                   yaxis_title='Price(USD$)',
                   title='Bitcoin Candlestick Chart Over Past 30 Days')
-
-
-# In[ ]:
-
 
 
 
